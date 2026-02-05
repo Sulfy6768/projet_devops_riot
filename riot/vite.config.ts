760c0c3e8ff06335dd131aff457d8a6582bda25b
Thomas-SuS <standage_thediet@8shield.net>
2026-02-05 17:15:35 +0100

    Fixed Github Pages

--- riot/vite.config.ts
+++ riot/vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/projet_devops_riot/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
